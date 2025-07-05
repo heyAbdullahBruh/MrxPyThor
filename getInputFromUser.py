cab20014addcc9c4d19efddf4781f595fb6fb3bc
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-05 20:50:36 +0600

    getInputFromUser.py

---
+++ getInputFromUser.py
@@ -0,0 +1,9 @@
+# Getting input form User using python-->
+
+name = input("Inter Your Name :")
+age = input("Inter Your age :")
+className = input("What class are you read in :")
+
+print("My name is "+name)
+print("I'm "+age+" Years old guy. ")
+print("Now I'm read in "+className)
