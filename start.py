31dd3b113e68665bf7929ea2adc45640b8febd95
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-05 20:16:16 +0600

    start.py1.0

--- start.py
+++ start.py
@@ -1,1 +1,10 @@
 print("Helllo Python")
+print(10)
+print("Hello I'm Thor .\n My Friend is Py.\nWe are Combine are Pythor. \t My age 1 and her age 2") # \n create new line and \t create multiple(4)  space or one tab
+
+# Python Comment --> 
+'''
+Python Have 2 Types Comment --> 
+1) Single Line Comment 
+2) Multiple Line COmment
+'''
