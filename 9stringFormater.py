a2ddccd1b9fbd02461104b90827c2c3dd73efc95
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-05 23:07:15 +0600

    string fromatter in python

---
+++ 9stringFormater.py
@@ -0,0 +1,20 @@
+# Python String formatter-->
+# If wanna format to return any string then Using F in print() fucn. like print(f"{num1}+ {num2} = {num1+num2}")
+
+
+'''
+firstName = "Abul "
+lastName ="Hasan"
+age=40
+title = "Daqtor"
+
+
+print(f"Our classes was a valo student who's name is {firstName}. His full name is {firstName + lastName}. He is {age} years old. He have a {age} kids .Now, kam kore as a {title} in Hello Haspatal")
+'''
+jobTitle = input("Enter Your Job title : ")
+firstName =input("Enter Your First name : ")
+lastName =input("Enter Your Last name : ")
+age=input("Enter Your Age : ")
+
+print(f"Hey There !\n This is {firstName} {lastName} .\n I'm {age} years old gay and work as a {jobTitle} in my company")
+
