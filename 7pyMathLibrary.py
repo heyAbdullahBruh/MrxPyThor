96eeb2908503e90c1e5f07332fab9b0e48a273e1
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-05 21:57:56 +0600

    python math library function

---
+++ 7pyMathLibrary.py
@@ -0,0 +1,15 @@
+# If you wanna use all of built in Math library func , at first import Math library from python . Let's start-->
+
+from math import * #math imported from python built in package.
+
+print(max(1,3,12,93,603,14,46,38,90)) #Return - 603, max func help to find maximum number from a number list
+print(min(1,3,12,93,603,14,46,38,90)) #Return - 1, min func help to find minimum number from a number list
+ 
+print(pow(3,2)) # Return - 9, pow func Return x**y (x to the power of y).
+print(sqrt(25)) # Return - 5, sqrt func Return the square root of x.
+print(ceil(9.2)) # Return - 10, celi func Return the largest number of X .
+print(floor(9.7))# Return - 9 , floor func Return the smallest number of X.
+print(factorial(5)) # Return - 120, factorial func return Factorial fo X
+print(sin(90)) #Return - 0.8939966636005579 , sin func Return the sine of x (measured in radians). x (measured in radians).
+
+
