7f19421d605584f62f9af0005692be24e3455ee4
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-05 20:29:14 +0600

    variable.py

---
+++ variable.py
@@ -0,0 +1,9 @@
+name1= "Thor"
+name2 ="Ek"
+age = 19
+gpa = 6.75
+
+print("I have a one valo friend. Halar name is "+name1)
+print("Hetar full name is  "+name1 + name2)
+print("Halar age is",age,"Years boyos")
+print("Halay got a nice gpa halar last final exam , halar gpa is ",gpa,"point")
