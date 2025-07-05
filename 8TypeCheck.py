e069f08e4087631f48684c488c7834c4451c2fb1
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-05 22:53:46 +0600

    python Typr function

---
+++ 8TypeCheck.py
@@ -0,0 +1,14 @@
+# Type fucntion check to variable value's type 
+
+name ='Hello PyThor'
+age =32
+cgpa =33.22
+isLoading =True
+paymentProcced =False
+
+print(type(name)) #Return : <class 'str'>
+print(type(age)) #Return :  <class 'int'>  
+print(type(cgpa)) #Return : <class 'float'>
+print(type(isLoading)) #Return : <class 'bool'> 
+print(type(paymentProcced)) #Return : <class 'bool'> 
+
