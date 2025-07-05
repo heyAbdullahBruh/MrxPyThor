46e9d8be702eababba3945b7b5f31cc75df3c82f
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-05 22:54:17 +0600

    python Type function

--- 8TypeCheck.py
+++ 8TypeCheck.py
@@ -4,11 +4,11 @@ name ='Hello PyThor'
 age =32
 cgpa =33.22
 isLoading =True
-paymentProcced =False
+paymentProcced =False 
 
-print(type(name)) #Return : <class 'str'>
-print(type(age)) #Return :  <class 'int'>  
-print(type(cgpa)) #Return : <class 'float'>
-print(type(isLoading)) #Return : <class 'bool'> 
-print(type(paymentProcced)) #Return : <class 'bool'> 
+print(type(name)) #Return :- <class 'str'>
+print(type(age)) #Return :-  <class 'int'>  
+print(type(cgpa)) #Return :- <class 'float'>
+print(type(isLoading)) #Return :- <class 'bool'> 
+print(type(paymentProcced)) #Return :- <class 'bool'> 
 
