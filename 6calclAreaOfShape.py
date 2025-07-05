3e7f8551df4103a321b74c394d8efeaed6dbfdb6
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-05 21:31:06 +0600

    Calculate area of shape using python

---
+++ 6calclAreaOfShape.py
@@ -0,0 +1,40 @@
+# Calculate area of shape using python --->
+
+# 1) Tringle shape's Area calculate--> 
+
+'''
+base = float(input("Enter tringle's Base :"))
+height = float(input("Enter tringle's height :"))
+
+areaOfTringle = .5 * base * height
+print("The area of Tringle Shape is :- ",areaOfTringle)
+'''
+
+# 2) Rectangle shape's Area calculate--> 
+
+'''
+width = float(input("Enter Rectangle's width :"))
+length = float(input("Enter Rectangle's length :"))
+
+areaOfRectangle = 2*(width+length)
+print("The area of Rectangle Shape is :- ",areaOfRectangle)
+'''
+
+
+# 3) Circle shape's Area calculate--> 
+'''
+breadth = float(input("Enter Circle's breadth :"))
+
+areaOfCircle = 3.1416 * breadth **2 
+print("The area of Circle Shape is :- ",areaOfCircle) 
+'''
+
+
+# 4) Traphiziam shape's Area calculate--> 
+
+width = float(input("Enter Traphiziam's width :"))
+length = float(input("Enter Traphiziam's length :"))
+vHeight = float(input("Enter Traphiziam's vertical Height :"))
+
+areaOfTraphiziam = .5 * (width + length) *vHeight 
+print("The area of Circle Shape is :- ",areaOfTraphiziam) 
