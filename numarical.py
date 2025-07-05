35ed17407f3f52f53f350e97db00954a1b0b37e9
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-05 20:41:31 +0600

    numarical.py

---
+++ numarical.py
@@ -0,0 +1,11 @@
+# numarical  operation in python . 
+
+a= 23
+b=4
+
+print(a+b)  
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) # // this operetor divided two number , but giving output by floor 
+print(a**b**a) # ** this operetor to help exponentiate numbers. 
