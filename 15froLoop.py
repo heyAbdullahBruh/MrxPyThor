d22bdaa0e3c94b8bdaaf403b959dcbefd132029c
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-06 17:31:53 +0600

    For loop

---
+++ 15froLoop.py
@@ -0,0 +1,23 @@
+#2 for loop --> 
+# For loop especialy use with list . 
+numbers = [10,20,44,5,66,777,88]
+
+'''
+i =1
+while i < len(numbers) :
+     print(numbers[i])
+     i=i+1 
+'''
+
+for item in numbers :
+    print(item)
+    
+    
+# Sum numbers value using for loop
+sum =0
+for item in numbers :
+    sum =sum +item
+print(sum) # 1010
+
+
+
