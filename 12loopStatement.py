3e7a7367a347b3c900b6db741c57ed6e295e17d3
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-06 16:00:03 +0600

    loop control statement

---
+++ 12loopStatement.py
@@ -0,0 +1,57 @@
+# Python Have two types loops control statement --->
+# 1) For Loop , 2) While Loop
+
+#i) While loop ---> 
+
+'''
+Rules OF While Loop -->
+
+i= start
+while condition :
+    body or result
+    iteration
+------->
+'''
+ 
+'''
+1)
+------>
+i =1
+while i <= 10:
+    # print (i)
+    print('Hello')
+    i=i+1   
+------>
+'''
+
+'''
+2)
+------>
+sum =0 
+i =1
+while i <= 10:
+    sum= sum+i
+    i=i+1   
+
+print(sum) #55
+------>
+'''
+
+# Break And Continue
+'''
+i =1
+while i <= 19:
+    if i ==10 :
+        break
+    print('Hello') #the output will print 9 times 
+    i=i+1 
+    
+i =1
+while i <= 19:
+    if i ==10 :
+        continue
+    print('Hello') #the output will print 9 times 
+    i=i+1 
+'''
+
+
