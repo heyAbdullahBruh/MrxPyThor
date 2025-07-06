9fbac87f6c44c031aad5175772c6aebfceed2bde
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-06 17:03:24 +0600

    Python Range Function

---
+++ 14RangeFunc.py
@@ -0,0 +1,13 @@
+# Range Function --> range function create multiple number items of list
+
+# 1)
+number = list(range(5)) # it's how many number creat from 0 
+print(number) # [0, 1, 2, 3, 4]
+
+# 2) 
+number2 =list(range(2,8))  # it's how many number create from X to Y ;eg : X =2(start) , Y =8(end)
+print(number2) # [2, 3, 4, 5, 6, 7]
+
+# 3) 
+number2 =list(range(2,50,5))  # it's how many number create from X to Y with how many step;eg : X =2(start) , Y =50(end) , 5(step)
+print(number2) # [2, 7, 12, 17, 22, 27, 32, 37, 42, 47]
