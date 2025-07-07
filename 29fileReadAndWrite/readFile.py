9faf53c7c862172bd66cd07593d67bafc99ea97b
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-07 18:14:41 +0600

    File read file python

---
+++ 29fileReadAndWrite/readFile.py
@@ -0,0 +1,15 @@
+#  File read and write with python --->
+
+file = open('hello.txt','r+')
+'''
+w=write file,r=read file , r+ = write / read files
+'''
+print(file.name) #hello.txt
+print(file.writable()) #True
+
+fileValues = file.read()
+print(fileValues.split('\n')) # split file value by line
+print(len(fileValues)) # get file length(character)
+
+
+
