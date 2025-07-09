1856b837102d4c0a03df6bd2c21cc1578a63c4d8
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-09 12:46:11 +0600

    Python Value Swapin

---
+++ 31Swappping.py
@@ -0,0 +1,19 @@
+# Value Swaping -->
+
+# old Way --->
+a="Shayed"
+b="Tarek"
+print(a,b) #Shayed Tarek
+temp =a
+a=b
+b=a
+print(a,b)#Shayed Tarek
+
+
+# easy way by python -->
+
+stu1= "Hasina"
+stu2= "Khaleda"
+print(stu1,stu2) #Hasina Khaleda
+(stu1,stu2)= (stu2 ,stu1)
+print(stu1,stu2)#Khaleda Hasina
