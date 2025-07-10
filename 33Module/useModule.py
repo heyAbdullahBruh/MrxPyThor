bc38ebde64e1a5da02a99243644f16e80a010bbd
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-10 23:37:13 +0600

    Python Module --> use module

---
+++ 33Module/useModule.py
@@ -0,0 +1,9 @@
+# Use module --> 
+
+from people import * # from people import Student,Worker
+
+student1 =Student('Abeed',25,'Inter 2nd Year','asbadh3432')
+student1.info()
+
+wrkr1 =Worker('Hasina',75,500000,'empsh45532')
+wrkr1.info()
