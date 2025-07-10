6c6360590eb8fcc255323486ecf477686cc1d4b8
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-10 19:49:58 +0600

    Python OOP -> inheritance3.0 examples

---
+++ 32oopPy/5exmplInteritance.py
@@ -0,0 +1,30 @@
+# example of class inheritence
+
+#Area calc-2--->
+
+class Shape :
+    base = 0
+    height = 0
+    def __init__(self,base,height):
+        self.base=base
+        self.height =height
+    
+class Tringle(Shape) :
+    
+    def calc(self) :
+        result =.5 * self.base * self.height
+        print(f"The Tringle shape's area is = {result} ")
+
+tringle1 =Tringle(20,40)
+tringle1.calc() #The Tringle shape's area is = 400.0 
+
+class Rectangle(Shape) :
+    
+    def calc(self) :
+        result = self.base * self.height
+        print(f"The Rectangle shape's area is = {result} ")
+        
+rec1 =Rectangle(30,50) 
+rec1.calc() #The Rectangle shape's area is = 1500 
+        
+        
