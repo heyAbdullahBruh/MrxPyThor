d6f5b9f988eac322a7e3e4d1d32fdf6562b61927
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-10 17:58:38 +0600

    Python OOP start

---
+++ 32oopPy/1class.py
@@ -0,0 +1,13 @@
+# Python object oriented programming start --- >
+
+class People :
+    name =''
+    phone =''
+aseef= People()
+
+print(isinstance(aseef,People)) # True ; check clsss 
+
+
+aseef.name = "Hazarat Asef"
+aseef.phone = "0182837234"
+print(f"Name = {aseef.name} , phone = {aseef.phone}") # Name = Hazarat Asef , phone = 0182837234
