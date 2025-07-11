5bc74e6e4213c4eb2363e8629dd13a6395b11585
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-11 22:53:18 +0600

    Python REGEX --> create a universal Pattern checker1.1

--- 34RegEx/universalChecker.py
+++ 34RegEx/universalChecker.py
@@ -4,4 +4,4 @@ def matchResult(pattern,text) :
     if re.match(pattern,text):
         print(f"{text} is a valid format")
     else : 
-        print('{text} is not a valid format')
+        print(f'{text} is not a valid format')
